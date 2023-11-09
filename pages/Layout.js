8a3e8408c93f05ae42c510ea018c01c147df7e83
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-11-09 16:42:37 -0500

    changes

--- pages/Layout.js
+++ pages/Layout.js
@@ -1,16 +1,9 @@
-import { Outlet, Link } from "react-router-dom";
 import React from "react"
-import Logo from "../assets/Marysbakinglogoremovebg.png"
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Hero from "../components/Hero"
-import Card from "../components/Card"
 import data from "../data"
-import Formpage from "../pages/Formpage"
-import Home from "../pages/Home";
-import NoPage from "../pages/NoPage";
-import AboutUs from "../pages/AboutUs";
+
 
 
 const Layout = () => {
